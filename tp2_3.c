436c4c29aa344fa601768ac92b10241cdb5e49a0
Rodrigo <rodrigomendoza721@gmail.com>
2024-04-28 17:19:10 -0300

    agregar el punto 3

---
+++ tp2_3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+
+#define N 5
+#define M 7
+
+int main(){
+    int i,j;
+    int mt[N][M];
+    int *punt;
+    punt = mt;
+
+    srand(time(NULL));
+    
+    for(i = 0;i<N; i++)
+    {
+        for(j = 0;j<M; j++)
+        {
+            *punt=1+rand()%100;
+            printf(" %d" , mt[i][j]);
+            punt++;
+        }
+    printf("\n");
+    }
+}
